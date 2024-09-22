ddab682bb5d5c4ee1bad24d143fb7e6667ed7862
anuragc3 <anuragc3@illinois.edu>
2024-09-22 17:58:07 -0500

    CIF Commit - 3

--- MP2/main.go
+++ MP2/main.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	"failure_detection/utility"
+	"fmt"
+	"os"
 )
+
+var LOGGER_FILE = "/home/log/machine.log"
 
 func main() {
 	// args := os.Args
+	// clearing the machine.log file
+	file, err := os.OpenFile(LOGGER_FILE, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		fmt.Println("Error Opening file : " + err.Error())
+	}
+
+	file.Close()
+
 	utility.LogMessage("Starting execution")
 	// typeArg is either "machine" or "caller", otherwise invalid
 
--- MP2/utility/utility.go
+++ MP2/utility/utility.go
@@ -13,10 +13,12 @@ var (
 	mu      sync.Mutex
 )
 
+var LOGGER_FILE = "/home/log/machine.log"
+
 func initLogger() {
 	once.Do(func() {
 		var err error
-		logFile, err = os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+		logFile, err = os.OpenFile(LOGGER_FILE, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
 		if err != nil {
 			log.Fatal(err)
 		}
