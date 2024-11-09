6c777507904dd2ef0360847c1a95e6334c8579da
akhilsundaram <akhils7@illinois.edu>
2024-11-09 00:55:41 -0600

    print qol

--- MP3/ring/misc.go
+++ MP3/ring/misc.go
@@ -112,11 +112,11 @@ func PrintRing() {
 		if err != nil {
 			utility.LogMessage("error getting file ranges while printing")
 		}
-		println(ringMember.serverName)
+		// println(ringMember.serverName)
 		if ringMember.serverName == membership.My_hostname {
-			fmt.Printf("host: %s, ringID: %d, successors: %d & %d, fileranges: [%d - %d]   <----------- Current Server\n", ringMember.serverName, ringMember.hashID, ringMember.successor[0], ringMember.successor[1], low, high)
+			fmt.Printf("host: %s, ringID: %d, successors: [%d, %d], fileranges: [%d - %d] <----\n", ringMember.serverName, ringMember.hashID, ringMember.successor[0], ringMember.successor[1], low, high)
 		} else {
-			fmt.Printf("host: %s, ringID: %d, successors: %d & %d, fileranges: [%d - %d] \n", ringMember.serverName, ringMember.hashID, ringMember.successor[0], ringMember.successor[1], low, high)
+			fmt.Printf("host: %s, ringID: %d, successors: [%d, %d], fileranges: [%d - %d] \n", ringMember.serverName, ringMember.hashID, ringMember.successor[0], ringMember.successor[1], low, high)
 		}
 	}
 }
