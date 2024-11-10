ff462091a4a169b09c6aba3d5275d3ef9f85af93
anuragc3 <anuragc3@illinois.edu>
2024-11-10 15:35:05 -0600

    changes ?

--- MP3/file_transfer/file_transfer.go
+++ MP3/file_transfer/file_transfer.go
@@ -418,7 +418,7 @@ func handleIncomingFileConnection(conn net.Conn) {
 		reqData.MultiAppend = true
 		reqData.Operation = "append"
 		go SendAppends(reqData, filename)
-		resp.Data = []byte("Append request from VM " + clientIp + "sent")
+		resp.Data = []byte("Append request from VM " + clientIp + " sent")
 		utility.LogMessage(string(resp.Data))
 
 	default:
