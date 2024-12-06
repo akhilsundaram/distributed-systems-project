511ed77b0262ebd5eeb79c9aa445ceb9b998bc7a
akhilsundaram <akhils7@illinois.edu>
2024-12-06 02:29:50 -0600

    protozon

--- MP4/stormworker/stormworkerserver.go
+++ MP4/stormworker/stormworkerserver.go
@@ -2,23 +2,36 @@ package stormworker
 
 import (
 	context "context"
+	"fmt"
 	"rainstorm/stormgrpc"
 	"rainstorm/utility"
 )
 
 // Server is the implementation of the StormWorker service.
-type StormorkerServer struct {
+type StormworkerServer struct {
 	stormgrpc.UnimplementedStormWorkerServer
 }
 
 // PerformOperation implements the PerformOperation RPC method.
-func (s *StormorkerServer) PerformOperation(ctx context.Context, req *stormgrpc.StormworkerRequest) (*stormgrpc.StormworkerResponse, error) {
+func (s *StormworkerServer) PerformOperation(ctx context.Context, req *stormgrpc.StormworkerRequest) (*stormgrpc.StormworkerResponse, error) {
 	// log.Printf("Received request: operation=%s, file=%s, range_start=%d, range_end=%d",
-	// 	req.Operation, req.InputFileName, req.RangeStart, req.RangeEnd)
+	// req.Operation, req.InputFileName, req.RangeStart, req.RangeEnd)
 
 	utility.LogMessage("Request received from leader - op: " + req.Operation + ",input: " + req.InputFileName)
 
-	// Simulated operation
+	tasks, err := addTask(int(req.Phase), req.Operation, int(req.RangeStart), int(req.RangeEnd), req.OutputFileName, req.InputFileName)
+	if err != nil {
+		utility.LogMessage(fmt.Sprintf("error trying to add new task - %v ", err))
+		status := "failure"
+		message := "Operation coulkd not be started because of " + err.Error()
+		return &stormgrpc.StormworkerResponse{
+			Status:  status,
+			Message: message,
+		}, nil
+
+	}
+
+	go runTask(tasks[int(req.Phase)]) // to be changed.! phase/stage cannot be the key
 	status := "success"
 	message := "Operation started successfully"
 
