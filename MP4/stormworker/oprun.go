1f189208b33ee7a6533ebc1bbbf8876510f06b04
akhilsundaram <akhils7@illinois.edu>
2024-12-09 11:55:15 -0600

    prints

--- MP4/stormworker/oprun.go
+++ MP4/stormworker/oprun.go
@@ -122,6 +122,7 @@ func RunOperation(task Task) {
 			if value, exists := processed[procKey]; exists {
 				if value >= lineNumber {
 					utility.LogMessage("DUPLICATE ENTRFY FOUND - skipping line => " + line)
+					fmt.Printf("DUPLICATE ENTRFY FOUND => " + line)
 					continue
 				}
 				pkey = procKey
